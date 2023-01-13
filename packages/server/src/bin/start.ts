41bee637a0dc2edb4aa2bea13046acc29786301a
Leon Si <leon@leondreamed.com>
2023-01-13 18:15:58 -0500

    change embed color

--- packages/server/src/bin/start.ts
+++ packages/server/src/bin/start.ts
@@ -4,11 +4,19 @@ import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat.js'
 import timezone from 'dayjs/plugin/timezone.js'
 import utc from 'dayjs/plugin/utc.js'
-import { ChannelType, Client, EmbedBuilder } from 'discord.js'
+import {
+	ChannelType,
+	Client,
+	EmbedBuilder,
+	Events,
+	GatewayIntentBits,
+} from 'discord.js'
 import dotenv from 'dotenv'
 import { fastify } from 'fastify'
 import { getProjectDir } from 'lion-utils'
 import invariant from 'tiny-invariant'
+
+import { type HabiticaRequest } from '~/types/habitica.js'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
@@ -20,73 +28,70 @@ const monorepoDir = getProjectDir(import.meta.url, { monorepoRoot: true })
 console.log(monorepoDir)
 dotenv.config({ path: path.join(monorepoDir, '.env') })
 
-const client = new Client({ intents: [] })
-client.on('ready', () => {
-	invariant(client.user !== null)
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+})
+
+client.once(Events.ClientReady, (client) => {
 	console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on(Events.MessageCreate, async (message) => {
+	const repliedTo = message.reference?.messageId
+	if (repliedTo === undefined) {
+		return
+	}
+
+	const needsProofMessage = await message.channel.messages.fetch(repliedTo)
+	const embed = needsProofMessage.embeds[0]
+	if (embed === undefined) {
+		return
+	}
+
+	const newEmbed = EmbedBuilder.from(embed)
+	newEmbed.setColor('Green')
+	newEmbed.addFields({
+		name: 'Proof',
+		value: message.url,
+	})
+
+	await needsProofMessage.edit({
+		embeds: [newEmbed],
+	})
 })
 
 const app = fastify()
 
-interface HabiticaRequest {
-	type: string
-	direction: string
-	delta: number
-	task: {
-		repeat: {
-			m: boolean
-			t: boolean
-			w: boolean
-			th: boolean
-			f: boolean
-			s: boolean
-			su: boolean
-		}
-		challenge: Record<string, unknown>
-		group: {
-			completedBy: Record<string, unknown>
-			assignedUsers: unknown[]
-		}
-		frequency: string
-		everyX: number
-		streak: number
-		nextDue: string[]
-		yesterDaily: true
-		history: Array<Record<string, unknown>>
-		completed: boolean
-		collapseChecklist: boolean
-		type: string
-		notes: string
-		tags: unknown[]
-		value: number
-		priority: number
-		attribute: string
-		byHabitica: boolean
-		startDate: string
-		daysOfMonth: unknown[]
-		weeksOfMonth: unknown[]
-		checklist: unknown[]
-		reminders: unknown[]
-		createdAt: string
-		updatedAt: string
-		_id: string
-		text: string
-		userId: string
-		isDue: boolean
-		id: string
-	}
-	user: Record<string, unknown>
-	webhookType: string
-}
-
 const notificationsChannelId = '1061299980792496202'
-const proofChannelId = '1061343393881530448'
-const leondreamedId = '1022267596382408755'
+
+const habiticaUserDataMap: Record<string, { discordId: string; name: string }> =
+	{
+		'984c33a5-7b36-478d-8e89-d8159fbafd95': {
+			name: 'Leon Si',
+			discordId: '1022267596382408755',
+		},
+		'kevins habitica id': {
+			name: 'Kevin Xu',
+			discordId: '723922029644087378',
+		},
+	}
 
 app.post('/webhook', async (request, reply) => {
 	console.log('Webhook called with:', request.body)
 	const data = request.body as HabiticaRequest
 	const { task } = data
+
+	const habiticaUserData = habiticaUserDataMap[data.user._id]
+	if (habiticaUserData === undefined) {
+		console.error(`Discord ID for Habitica user ${data.user._id} not found.`)
+		return reply.status(400)
+	}
+
+	const { discordId, name } = habiticaUserData
 
 	let title: string
 	let description: string
@@ -98,17 +103,23 @@ app.post('/webhook', async (request, reply) => {
 		description = 'A task has been unchecked!'
 	}
 
+	const doesTaskNeedProof =
+		/\*\*Needs Proof:\*\* (.*)/.test(task.notes) && data.direction === 'up'
 	const embed = new EmbedBuilder()
-		.setColor('Orange')
+		.setColor(doesTaskNeedProof ? 'Orange' : 'Green')
 		.setTitle(title)
 		.setDescription(description)
 		.addFields(
 			{
-				name: 'Name',
+				name: 'User',
+				value: name,
+			},
+			{
+				name: 'Task Name',
 				value: task.text,
 			},
 			{
-				name: 'Notes',
+				name: 'Task Notes',
 				value: task.notes,
 			},
 			{
@@ -122,24 +133,22 @@ app.post('/webhook', async (request, reply) => {
 	)
 	invariant(notificationsChannel?.type === ChannelType.GuildText)
 
-	await notificationsChannel.send({ embeds: [embed] })
-
-	if (task.notes.includes('Needs Proof:') && data.direction === 'up') {
+	if (doesTaskNeedProof) {
 		const proofDescription = /\*\*Needs Proof:\*\* (.*)/.exec(task.notes)?.[1]
 		invariant(proofDescription !== undefined, 'missing proof item')
-		const proofChannel = await client.channels.fetch(proofChannelId)
-		invariant(proofChannel?.type === ChannelType.GuildText)
-		await proofChannel.send(
-			`<@${leondreamedId}>, please send proof of completion for your task _${task.text}_ (${proofDescription})`
+		const notificationsChannel = await client.channels.fetch(
+			notificationsChannelId
 		)
-	} else if (task.notes.includes('Proof:') && data.direction === 'up') {
-		const proofDescription = /\*\*Proof:\*\* (.*)/.exec(task.notes)?.[1]
-		invariant(proofDescription !== undefined, 'missing proof item')
-		const proofChannel = await client.channels.fetch(proofChannelId)
-		invariant(proofChannel?.type === ChannelType.GuildText)
-		await proofChannel.send(
-			`Proof of completion for task _${task.text}_: ${proofDescription}`
-		)
+		invariant(notificationsChannel?.type === ChannelType.GuildText)
+		console.log('sending proof message')
+		await notificationsChannel.send({
+			embeds: [embed],
+			content: `<@${discordId}>, please send proof of completion for your task _${task.text}_ (${proofDescription})`,
+		})
+	} else {
+		await notificationsChannel.send({
+			embeds: [embed],
+		})
 	}
 
 	console.debug('Webhook finished!')
---
+++ packages/server/src/types/habitica.ts
@@ -0,0 +1,70 @@
+export interface HabiticaRequest {
+	type: string
+	direction: string
+	delta: number
+	task: {
+		repeat: {
+			m: boolean
+			t: boolean
+			w: boolean
+			th: boolean
+			f: boolean
+			s: boolean
+			su: boolean
+		}
+		challenge: Record<string, unknown>
+		group: {
+			completedBy: Record<string, unknown>
+			assignedUsers: unknown[]
+		}
+		frequency: string
+		everyX: number
+		streak: number
+		nextDue: string[]
+		yesterDaily: true
+		history: Array<Record<string, unknown>>
+		completed: boolean
+		collapseChecklist: boolean
+		type: string
+		notes: string
+		tags: unknown[]
+		value: number
+		priority: number
+		attribute: string
+		byHabitica: boolean
+		startDate: string
+		daysOfMonth: unknown[]
+		weeksOfMonth: unknown[]
+		checklist: unknown[]
+		reminders: unknown[]
+		createdAt: string
+		updatedAt: string
+		_id: string
+		text: string
+		userId: string
+		isDue: boolean
+		id: string
+	}
+	user: {
+		_id: string
+		stats: {
+			buffs: Record<string, unknown>
+			training: Record<string, unknown>
+			hp: number
+			mp: number
+			exp: number
+			gp: number
+			lvl: number
+			class: string
+			points: number
+			str: number
+			con: number
+			int: number
+			per: number
+			toNextLevel: number
+			maxHealth: number
+			maxMP: number
+		}
+		webhookType: string
+	}
+}
