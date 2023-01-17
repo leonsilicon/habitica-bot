9f99eee7bc1f76101c6cd5e942d236b884db9a5f
Leon Si <leon@leondreamed.com>
2023-01-17 09:26:33 -0500

    cascade delete

---
+++ packages/server/migrations/20230117142626_cascade/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "discordUserId" TEXT NOT NULL,
+    "habiticaUserId" TEXT NOT NULL,
+    "areTasksPublic" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "User_habiticaUserId_fkey" FOREIGN KEY ("habiticaUserId") REFERENCES "HabiticaUser" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_User" ("areTasksPublic", "discordUserId", "habiticaUserId", "id") SELECT "areTasksPublic", "discordUserId", "habiticaUserId", "id" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_discordUserId_key" ON "User"("discordUserId");
+CREATE UNIQUE INDEX "User_habiticaUserId_key" ON "User"("habiticaUserId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
